175cf3a2463e46a9dc251fda71fca2958640c9d4
noel7781 <noel7781@naver.com>
2022-04-28 13:24:56 +0900

    0428 backjoon

--- Backjoon/21757.cpp
+++ Backjoon/21757.cpp
@@ -4,24 +4,10 @@ typedef long long ll;
 int N;
 vector<int> V;
 int psum[100002];
-ll cache[100002][5];
-ll solve(int pos, int left) {
-    if(left == 0 && pos == 0) return 1LL;
-    if(left == 0) return 0LL;
-    ll& ret = cache[pos][left];
-    if(ret != -1) return ret;
-    ret = 0;
-    for(int p = 0; p < pos; p++) {
-        if(psum[pos] - psum[p] == psum[N]/4) {
-            ret += solve(p, left-1);
-        }
-    }
-    return ret;
-}
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    memset(cache, -1, sizeof(cache));
     cin >> N; V.resize(N+1); 
+    vector<int> one, two, three;
     for(int i = 1; i <= N; i++) {
         cin >> V[i];
         psum[i] = psum[i-1] + V[i];
@@ -31,7 +17,18 @@ int main() {
         cout << 0 << "\n";
         return 0;
     }
-    int part = total / 4;
-    ll ret = solve(N, 4);
+    for(int i = 1; i < N; i++) {
+        if(psum[i] == total/4) one.push_back(i);
+        if(psum[i] == total*2/4) two.push_back(i);
+        if(psum[i] == total*3/4) three.push_back(i);
+    }
+    ll ret = 0;
+    for(auto &f: one) {
+        for(auto s = upper_bound(two.begin(), two.end(), f); s != two.end(); s++) {
+            ret += three.end() - upper_bound(three.begin(), three.end(), *s);
+        }
+
+    }
+
     cout << ret << "\n";
 }
