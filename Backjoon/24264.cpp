5aed1304a0cd51982ce93dc8ffc697487ee32d3f
noel7781 <noel7781@naver.com>
2022-07-16 22:43:26 +0900

    0716 backjoon

---
+++ Backjoon/24264.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    long long N; cin >> N;
+    cout << N*N << "\n";
+    cout << 2 << "\n";
+}
